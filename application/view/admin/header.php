857c77c824073aeaaf8a5fd9e3b39c54c1709b31
adalenv <adalenvladi@gmail.com>
2018-04-29 23:07:54 +0200

    fix perfectscrollbar

--- application/view/admin/header.php
+++ application/view/admin/header.php
@@ -43,7 +43,6 @@
 
         Tip 2: you can also add an image using data-image tag
     --> 
-    <?php  echo $_SESSION['role']; ?> 
             <div class="logo">
                 <a href="http://www.creative-tim.com" class="simple-text">
                     Contra
--- public/assets/js/material-dashboard.js
+++ public/assets/js/material-dashboard.js
@@ -14,19 +14,18 @@
 
  */
 
-// (function() {
+$(document).ready(function(){
      isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-//     if (isWindows) {
-//         // if we are on windows OS we activate the perfectScrollbar function
-       // $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
+    if (isWindows) {
+        // if we are on windows OS we activate the perfectScrollbar function
+       $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
 
-       // $('html').addClass('perfect-scrollbar-on');
-//     } else {
-//         $('html').addClass('perfect-scrollbar-off');
-//     }
-// })();
-
+       $('html').addClass('perfect-scrollbar-on');
+    } else {
+        $('html').addClass('perfect-scrollbar-off');
+    }
+});
 
 var searchVisible = 0;
 var transparent = true;
