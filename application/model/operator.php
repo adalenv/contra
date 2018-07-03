c28c63e5d958f64be8ac03219e8db0466b9e56f5
Adalen Vladi <adalenvladi@gmail.com>
2018-07-03 13:08:02 +0200

    Update operator.php

--- application/model/operator.php
+++ application/model/operator.php
@@ -626,7 +626,7 @@ class Model
                     if (!in_array($ext,$allow_ext)) { 
                         echo "ext_error: ";
                         echo $ext;
-                        return;
+                        
                     }
                     if (move_uploaded_file($_FILES["file"]["tmp_name"],$target_file1)) {
                         $sql="INSERT INTO audios(contract_id,url) VALUES(:contract_id,:url)";
