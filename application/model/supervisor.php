341209e01a33672bc4dbc4a1487887ebf9bdef48
adalenv <adalenvladi@gmail.com>
2018-08-27 16:57:56 +0200

    60 day supervisor limit contracts

--- application/model/supervisor.php
+++ application/model/supervisor.php
@@ -251,7 +251,11 @@ class Model
                     AND vat_number LIKE :codice_fiscale
                     AND supervisor=:supervisor_id
                     AND (
-                            MONTH(`date`) = MONTH(CURRENT_DATE())
+                            (
+                                MONTH(`date`) = MONTH(CURRENT_DATE())
+                            OR
+                                MONTH(`date`) = MONTH(CURRENT_DATE())-1
+                            )
                         AND 
                            YEAR(`date`) = YEAR(CURRENT_DATE())
                         )
--- application/view/admin/footer.php
+++ application/view/admin/footer.php
@@ -1,4 +1,4 @@
- <footer class="footer">
+            <footer class="footer">
 
                     <p class="copyright pull-right">
                         &copy;
@@ -8,6 +8,10 @@
                         <a style="margin-right:15px" href="#">Adalen VLADI</a> <!-- , made with love for a better web -->
                     </p>
                 </div>
+                <!-- start orangechat code -->
+                <link type="text/css" rel="stylesheet" media="all" href="http://192.168.1.222/orangechat/orangechat/orangecss.php" />
+                <script type="text/javascript" src="http://192.168.1.222/orangechat/orangechat/orangejs.php"></script>
+                <!-- end orangechat code -->
             </footer>
         </div>
     </div>
