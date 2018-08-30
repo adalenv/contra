6051e1e6014583f4beb070fd33585dad811dd142
adalenv <adalenvladi@gmail.com>
2018-08-30 10:42:06 +0200

    fix filter

--- application/model/admin.php
+++ application/model/admin.php
@@ -494,12 +494,10 @@ class Model
 			}else{                          // if one part
 				$first_name=$client_name.'%';
 				$last_name='%';
-                $last2='%';
 			}	
 		} else {						//if none
 				$first_name='%';
 				$last_name='%';
-                $last2='%';
 		}
         ///////////////////////////////////////////////////////////////////
         
