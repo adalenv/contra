26015cfa9fb2cf6280d9418f195dfa0eb0eef579
adalenv <adalenvladi@gmail.com>
2018-11-06 12:14:59 +0100

    vici backup

--- auto_backup.sh
+++ auto_backup.sh
@@ -1,6 +1,7 @@
 rsync --ignore-existing  /var/www/html/application/audios/* /backup/audios ;
 rsync --ignore-existing  /var/www/html/application/documents/* /backup/documents ;
 /usr/bin/mysqldump  -pAdmin2018  justcall > /backup/database/justcall.backup_$(date +%Y%m%d).sql ; 
-/usr/bin/find  /backup/database/ -type f -mtime +7 -print | xargs rm -f ;
+/usr/bin/mysqldump -h192.168.2.2  -ucron -p1234  asterisk > /backup/database/vicidial.backup_$(date +%Y%m%d).sql ;
+/usr/bin/find  /backup/database/ -type f -mtime +1 -print | xargs rm -f ;
 
 echo 'Backup success!';
