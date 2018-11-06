31f7180ef7cbea5bf748646f8e5394582009eb84
adalenv <adalenvladi@gmail.com>
2018-11-06 11:56:42 +0100

    rm  -7 d

--- auto_backup.sh
+++ auto_backup.sh
@@ -1,4 +1,6 @@
 rsync --ignore-existing  /var/www/html/application/audios/* /backup/audios ;
 rsync --ignore-existing  /var/www/html/application/documents/* /backup/documents ;
 /usr/bin/mysqldump  -pAdmin2018  justcall > /backup/database/justcall.backup_$(date +%Y%m%d).sql ; 
+/usr/bin/find  /backup/database/ -type f -mtime +7 -print | xargs rm -f ;
+
 echo 'Backup success!';
